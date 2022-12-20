e051b010cb0338d2ee8e32e2bfc4d1a01e6102aa
Nuwan-Wi <nuwanwijeweera@gmail.com>
2022-12-20 14:33:57 +0530

    SignIn & SignUp Created

--- src/Pages/signin.js
+++ src/Pages/signin.js
@@ -4,6 +4,9 @@ import { Card, Divider, } from '@mui/material';
 import { blue, grey, deepPurple, yellow } from '@mui/material/colors';
 import Google from './../images/logo.png';
 import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
@@ -17,8 +20,8 @@ function TabPanel(props) {
       <div
         role="tabpanel"
         hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
         {...other}
       >
         {value === index && (
@@ -29,26 +32,33 @@ function TabPanel(props) {
       </div>
     );
   }
+  
   TabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.number.isRequired,
     value: PropTypes.number.isRequired,
   };
-
+  
   function a11yProps(index) {
     return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
+      id: `full-width-tab-${index}`,
+      'aria-controls': `full-width-tabpanel-${index}`,
     };
   }
   
 
 function SignIn() {
+    const theme = useTheme();
     const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    const handleChangeIndex = (index) => {
+      setValue(index);
+    };
+  
 
 
     return (
@@ -57,7 +67,6 @@ function SignIn() {
             <Container sx={{
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'center',
             alignItems: 'center',
             minHeight: '100vh',
             minWidth: '100vw',
@@ -68,51 +77,98 @@ function SignIn() {
                 p: 5,
                 borderRadius: '7px',
                 display: 'flex',
-                
+                marginTop: '50px',
+                height:'75vh'                
             }}>
 
-            <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="Login" {...a11yProps(0)} />
-                <Tab label="Register" {...a11yProps(1)} />
-                </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <div id="frame">
-                <h2>Welcome Back!</h2>
-                <label id="p1">Login to Account</label>
-                <div className='login-btn'>
-                <button className='social-media-btn' itemType="button">Login with Google</button>
-                <button className='social-media-btn'>Login with Facebook</button>
-                </div>
-                <p id="or">OR</p>
+            <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
+                {/*<AppBar position="static">
+                    <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="#334"
+                    textColor="inherit"
+                    variant="fullWidth"
+                    bgcolor="#000"
+                    >
+                    <Tab label="LogIn" {...a11yProps(0)} />
+                    <Tab label="Register" {...a11yProps(1)} />
+                    </Tabs>
+        </AppBar>*/}
+                <SwipeableViews
+                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                    index={value}
+                    onChangeIndex={handleChangeIndex}
+                >
+                    <TabPanel value={value} index={0} dir={theme.direction}>
+                    <div id="frame">
+                    <h2>Welcome Back!</h2>
+                    <label id="p1">Login to Account</label>
+                    <div className='login-btn'>
+                    <button className='social-media-btn' itemType="button">Login with Google</button>
+                    <button className='social-media-btn'>Login with Facebook</button>
+                    </div>
+                    <p id="p1">OR</p>
 
-                <div className="E-mail">                
-                <lable id="email">E-mail :</lable>
-                <input id="Email"></input>                
-                </div>
+                    <div className="E-mail">                
+                    <lable id="email">E-mail :</lable>
+                    <input id="Email"></input>                
+                    </div>
 
-                <div className='line'></div>                
-                
-                <div className="pwd">
-                <label id="password">Password :</label>
-                <input id="Password"></input>                
-                </div>
+                    <div className='line'></div>                
+                    
+                    <div className="pwd">
+                    <label id="password">Password :</label>
+                    <input id="Password"></input>                
+                    </div>
 
-                <div className='line'></div>
+                    <div className='line'></div>
 
-                <button className='bttn' style={{width:'350px', marginTop: '20px', fontSize:'20px' }}>Login</button>
-                <label className='p2'>Don't have an account.. <a className='Register' href="">Register</a></label>
+                    <button className='bttn' style={{width:'350px', marginTop: '20px', fontSize:'20px' }}>Login</button>
+                    <label className='p2'>Don't have an account.. <a className='Register' onClick= {()=>handleChange("index",1)}>Register</a></label>
 
-            </div>  
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-    </Box>      
-      </Card>
-            </Container>
+                </div>  
+                    </TabPanel>
+                    <TabPanel value={value} index={1} dir={theme.direction}>
+                    <div id="frame">
+                    <h2>You are most Welcome!</h2>
+                    <label id="p1">Register</label>
+                    <div className='login-btn'>
+                    <button className='social-media-btn' itemType="button">Register with Google</button>
+                    <button className='social-media-btn'>Register with Facebook</button>
+                    </div>
+                    <p id="p1">OR</p>
+
+                    <div className="E-mail">                
+                    <lable id="email">E-mail :</lable>
+                    <input id="Email"></input>                
+                    </div>
+
+                    <div className='line'></div>                
+                    
+                    <div className="pwd">
+                    <label id="password">Password :</label>
+                    <input id="Password"></input>                
+                    </div>
+                    
+                    <div className='line'></div>
+
+                    <div className="pwd">
+                    <label id="password">Re-Enter Password :</label>
+                    <input id="Password"></input>                
+                    </div>
+
+                    <div className='line'></div>
+
+                    <button className='bttn' style={{width:'350px', marginTop: '20px', fontSize:'20px' }}>Register</button>
+                    <label className='p2'>Already have an account.. <a className='Register' onClick= {()=>handleChange("index",0)}>SignUp</a></label>
+
+                </div>  
+                </TabPanel>
+                </SwipeableViews>
+            </Box>      
+        </Card>
+        </Container>
         </div>
       );
 }
