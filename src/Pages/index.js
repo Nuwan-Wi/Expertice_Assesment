de414b8f4e3fd4a7df826d0b14916a3b1840be13
Nuwan-Wi <nuwanwijeweera@gmail.com>
2023-02-07 22:55:32 +0530

    Featuered cards added

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -9,6 +9,9 @@ import SwipeToSlide from '../Components/ImageSlider/imageSlider'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Footer from '../Components/Footer/footer';
+import hotel1im1 from "../images/Resorvations/Hotel_01_1.jpg";
+import Sigiriya from "../images/sigiriya.jpg";
+import StarRatingComponent from 'react-star-rating-component';
 
 
 function Home(props) {
@@ -42,11 +45,185 @@ function Home(props) {
         
 
         <div className='index-body'>
-        <h1>Destinations we are providing our services</h1>
-        <h3>Trending Tours</h3>
-        
+        <h1>WE ARE OFFERING THE HOTTEST OFFERS</h1>
+        <p>looking for place to stay at the night.</p>
+        <p> Here is the solution</p>
 
-        </div>  
+        <div style={{display:"flex", flexDirection:'row', justifyContent:'space-between' }}>
+        <div className='main_page_card'>
+        <img src={hotel1im1} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+        
+        <div className='main_page_card'>
+        <img src={hotel1im1} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+        
+        <div className='main_page_card'>
+        <img src={hotel1im1} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+
+        <div className='main_page_card'>
+        <img src={hotel1im1} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+        
+        </div> 
+
+        </div>
+        
+        <div className='index-body'>
+        <h1>OUR FEATURED TOURS</h1>
+        <p>looking for place to stay at the night.</p>
+        <p> Here is the solution</p>
+
+        <div style={{display:"flex", flexDirection:'row', justifyContent:'space-between' }}>
+        <div className='main_page_card'>
+        <img src={Sigiriya} style={{width:'300px', height:'250px'}}/>
+        <h3>Sigiriya</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+        
+        <div className='main_page_card'>
+        <img src={Sigiriya} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+        
+        <div className='main_page_card'>
+        <img src={Sigiriya} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+
+        <div className='main_page_card'>
+        <img src={Sigiriya} style={{width:'300px', height:'250px'}}/>
+        <h3>Hotel Kandalama</h3>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <div>
+        <p>Featured</p>
+        <StarRatingComponent
+        name="rate1"
+        starCount={5}
+        value={4}
+        />
+        </div>
+        <div style={{width:'2px',backgroundColor:'#039d24',marginRight:'-120px'}}/>
+        <div>
+        <h2 style={{color:"#039d24",marginTop:'-1px'}}>3500Rs</h2>
+        <p style={{fontSize:'12px',}}>Cost per day</p>
+        </div>
+        </div>
+        </div>
+        
+        </div> 
+
+        </div>
+
         <Footer/>      
         </div>
     );
